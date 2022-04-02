b3982d8a324677f2d71aa18b69c17f9d7098e48f
WTS <webtechsolutionn@gmail.com>
2022-04-02 11:41:54 +0530

    toast

--- app/src/main/java/com/example/android/navigation/MainActivity2.kt
+++ app/src/main/java/com/example/android/navigation/MainActivity2.kt
@@ -2,10 +2,12 @@ package com.example.android.navigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        Toast.makeText(this,"santosh you are great",Toast.LENGTH_LONG).show()
     }
 }
